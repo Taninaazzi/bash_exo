28bc8b2a1e1b9f970b0fd465fb2acba57b79285c
Taninaazzi <124151682+Taninaazzi@users.noreply.github.com>
2023-02-01 15:02:43 +0100

    Add files via upload

---
+++ pinger.sh
@@ -0,0 +1,18 @@
+ip=10.0.2
+online_count=0
+offline_count=0
+
+for i in {1..255}
+do
+ping -c 1 $ip.$i
+if [ $? = 0 ]
+then
+echo $ip.$i >> /home/kali/Desktop/online.txt
+online_count=$((online_count+1))
+else
+echo $ip.$i >> /home/kali/Desktop/offline.txt
+offline_count=$((offline_count+1))
+fi
+done
+
+echo "Number of online hosts: $online_count"
